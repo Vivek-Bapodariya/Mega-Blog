257845e738eeaa5aad6e9ee3794f456d77267f57
Vivek-Bapodariya <vtbvtb412@gmail.com>
2024-01-21 13:54:03 +0530

    Add Logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Logo({width='100px'}) {
   return (
-    <div>Logo</div>
+               <div class='w-15 h-5'>
+                <img src="src\components\Logo\Logo.MegaBlogO.jpg"></img>
+              </div> 
+
+
+              
   )
 }
 
