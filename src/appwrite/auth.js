947857cdcc0ca162271cbad672432e77256ac875
Vivek-Bapodariya <vtbvtb412@gmail.com>
2024-01-23 20:20:25 +0530

    Add minor change

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -56,4 +56,4 @@ export class AuthService {
 
 
 const authService = new AuthService();
-export default authService;
+export default authService
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
-function Logo({width='100px'}) {
+//function Logo({width='100px'})  remove curly bracket
+function Logo() {
   return (
-               <div class='w-15 h-5'>
+               <div className='w-15 h-5'>
                 <img src="src\components\Logo\Logo.MegaBlogO.jpg"></img>
               </div> 
 
