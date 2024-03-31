ce78438a2f4d6f405b02dbb149bc30286644475b
Vivek-Bapodariya <vtbvtb412@gmail.com>
2024-03-31 17:18:46 +0530

    logo add

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,12 +2,9 @@ import React from 'react'
 //function Logo({width='100px'})  remove curly bracket
 function Logo() {
   return (
-               <div className='w-15 h-5'>
-                <img src="src\components\Logo\Logo.MegaBlogO.jpg"></img>
-              </div> 
-
-
-              
+    <div className='w-15 h-5'>
+      <img src="src\components\Logo\Logo.MegaBlogO.jpg"></img>
+    </div>
   )
 }
 
